2397a837095021b42e044fb134f9bdaf27513a87
Chris Kennedy <c@groovy.org>
2023-10-25 01:34:56 -0700

    add base llm
    TODO: finish up zmqp output of llm

---
+++ lifeAIllm/build-info.h
@@ -0,0 +1,9 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define BUILD_NUMBER 1338
+#define BUILD_COMMIT "1faaae8"
+#define BUILD_COMPILER "Apple clang version 14.0.0 (clang-1400.0.29.202)"
+#define BUILD_TARGET "x86_64-apple-darwin22.6.0"
+
+#endif // BUILD_INFO_H
