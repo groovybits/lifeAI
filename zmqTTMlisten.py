cdba271b0a039d0b3d6ef61b80c3c07745c2eeb5
Chris Kennedy <c@groovy.org>
2023-10-28 17:58:41 -0700

    lower volume of sound effects

--- zmqTTMlisten.py
+++ zmqTTMlisten.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     parser.add_argument("--save_file", action="store_true", help="Save the received audio as WAV files")
     parser.add_argument("--show_hex", action="store_true", help="Show the hex representation of the audio payload")
     parser.add_argument("--audio_format", type=str, choices=["wav", "raw"], default="wav", help="Audio format to save as. Choices are 'wav' or 'raw'. Default is 'wav'.")
-    parser.add_argument("--volume", type=float, default=0.6, help="Playback volume (0.0 to 1.0, default is 0.6)")
+    parser.add_argument("--volume", type=float, default=0.4, help="Playback volume (0.0 to 1.0, default is 0.6)")
     args = parser.parse_args()
 
     context = zmq.Context()
