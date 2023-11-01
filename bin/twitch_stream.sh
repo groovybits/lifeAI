dacf9e73ec0ec8fa6466252317778fdae38363a2
Chris Kennedy <c@groovy.org>
2023-10-31 19:57:46 -0700

    add twitch streaming startup using ffmpeg
    No more OBS!!!

---
+++ bin/twitch_stream.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Stream on MacOS to twitch
+# Get your stream key and add it, not here but use env values
+#
+## add value to .env
+source .env
+#TWITCH_STREAM_KEY=
+VIDEO_DEV=0
+AUDIO_DEV=1
+
+ffmpeg -y -hide_banner \
+    -probesize 500M -pix_fmt uyvy422 \
+    -f avfoundation -i $VIDEO_DEV:$AUDIO_DEV \
+    -loglevel warning \
+    -pix_fmt yuv420p \
+    -vcodec h264_videotoolbox \
+    -preset slow \
+    -b:v 3000k \
+    -acodec aac -ar 48000 \
+    -b:a 192k -ac 2 \
+    -maxrate 3000k -minrate 3000k -bufsize 3000k \
+    -g 60 \
+    -keyint_min 30 \
+    -flags:v +global_header \
+    -flags:a +global_header \
+        -f flv \
+            rtmp://sfo.contribute.live-video.net/app/$TWITCH_STREAM_KEY
