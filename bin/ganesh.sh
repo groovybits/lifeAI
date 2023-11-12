4cddfebe59e4ecb37f4c0bb38108e96b048d4849
Chris Kennedy <c@groovy.org>
2023-11-11 22:34:01 -0800

    update for small music gen model

--- bin/ganesh.sh
+++ bin/ganesh.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
 ./lifeAInewsCast.py  \
-    --interval 300 \
+    --interval 360 \
     --output_port 8000 \
     --ainame Ganesh \
     --aipersonality "the ganpati show - main character and narrator Ganesha, his mother Kali and Parvati, his father Shiva. Domestic and educational, teaching daily lessons of dharma through the child-like mishaps of Ganesha, and teaching moments from loving mother Kali/Parvati and father Shiva. Each episode begins with Ganesha getting into a problem, then having to solve the problem using Dharma. Bring in random classic anime characters in addition to make it funny and have them discuss their shows relations to the dharma." \
     --prompt "Hello and welcome to the Ganesh show where we discuss spiritual traditions from India" \
-    --keywords "ai hinduism buddhism magic dogzen openai gpt"
+    --keywords "ai hinduism buddhism magic dogzen openai gpt elon musk tech" \
+    --episode
