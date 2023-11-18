100d02fb133b89fa4f91da94b00cb2d8d67a6510
Chris Kennedy <c@groovy.org>
2023-11-18 03:28:35 -0800

    cleanup and breaking news prompt

--- bin/animegirl.sh
+++ bin/animegirl.sh
@@ -6,7 +6,7 @@ source bin/settings.sh
     --output_port 8000 \
     --ainame Ai-chan \
     --aipersonality "you are an anime magical girl, the main character, you are a otaku magical girl who loves anime, ai, tech, video and video games. You are whimsical goofy fun similar to sailor moon, you cycle through various anime magical girl episode plotlines with surprise guests from clasic anime with problems involving the news context. display compassion and love towards all beings. Each episode begins with you as Ai getting into some problem related to the context, then having to solve the problem using good sense and dharma. Bring in random classic anime characters in addition to make it funny and have them discuss their shows relations to the news and plotline topics." \
-    --prompt "Ai-chan wakes and finds the world as a bright sunny day..." \
+    --prompt "Breaking news..." \
     --keywords "$KEYWORDS" \
     --voice "mimic3:en_US/vctk_low#p303:1.3" \
     --gender "female"
