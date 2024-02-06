3c3463d5f36bc0bd48739a0afa6fc9ad63f712e6
Chris Kennedy <c@groovy.org>
2024-02-05 22:17:08 -0800

    sort music from oldest to newest
    generated in order using each as a sample for the next one,
    so order them as created for continuity.

--- bin/youtube_music.sh
+++ bin/youtube_music.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-while true; do
-    for file in /Volumes/BrahmaSSD/music/AiGen/*.*; do
-        echo "Playing '$file'"
-        mpv "$file" --audio-device="coreaudio/com.rogueamoeba.Loopback:FB46673C-58FA-4556-986F-7986B4D8C4E5"
-        sleep 1
-    done
+# Use find to list files, then pipe to ls to sort by modification time
+playlist=$(find /Volumes/BrahmaSSD/music/AiGen/ -type f -exec ls -tr {} +)
+
+# Play sorted playlist with mpv
+for file in $playlist; do
+    echo "Playing '$file'"
+    mpv "$file" --audio-device="coreaudio/com.rogueamoeba.Loopback:FB46673C-58FA-4556-986F-7986B4D8C4E5" # Add your mpv options here
 done
 
